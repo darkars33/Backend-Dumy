a3aa5216ee5ae3614a739cdfde52f6d11fd93e57
darkars33 <darshangarad18@gmail.com>
2024-08-18 00:28:39 +0530

    minor updates

--- routes/userAuth.route.js
+++ routes/userAuth.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const {signup, login, logout, getMe} = require('../controllers/userAuth.controller');
 const protectRoute = require('../middleware/protectRoute');
 
-router.post('/signup', signup);
+router.post('/createAccount', signup);
 router.post('/login', login);
 router.post('/logout', logout);
 router.get('/profile', protectRoute, getMe);
